fb30c7a9e1cd3acab4c3c60ac566f354d2e9cf2b
Diego Ortiz <dortizibert@ufl.edu>
2025-11-13 16:50:22 -0500

    merge changes

--- test_calculator.py
+++ test_calculator.py
@@ -12,12 +12,13 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(-5, -5), -10)
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(3, 4), 12)
+        self.assertEqual(mul(-2, 5), -10)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self): # 3 assertions
+        self.assertEqual(div(10, 2), 5)
+        self.assertAlmostEqual(div(7, 3), 7 / 3)
 
     ####### Partner 2
     def test_divide_by_zero(self):
@@ -33,22 +34,26 @@ class TestCalculator(unittest.TestCase):
             log(10, 1)
 
     ####### Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
-    #
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            log(-5, 10)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    # #########################
+        with self.assertRaises(ValueError):
+            log(10, 1)
+
+        with self.assertRaises(ValueError):
+            log(10, -2)
+
+    def test_hypotenuse(self): # 3 assertions
+        self.assertAlmostEqual(hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(hypotenuse(5, 12), 13)
+
+    def test_sqrt(self): # 3 assertions
+        self.assertEqual(square_root(9), 3)
+        self.assertEqual(square_root(0), 0)
+
+        with self.assertRaises(ValueError):
+            square_root(-1)
 
 # Do not touch this
 if __name__ == "__main__":
